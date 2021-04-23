bada7c9c517daf173bdf99788a767ab2b1c12e9a
Gen Schmitt <gschmitt@illinois.edu>
2021-04-23 14:12:51 -0500

    Changed error reporting from a hash to an array and added error type

--- lib/extractor.rb
+++ lib/extractor.rb
@@ -5,14 +5,15 @@ require 'json'
 
 require_relative 'extractor/extraction.rb'
 require_relative 'extractor/extraction_status.rb'
+require_relative 'extractor/error_type.rb'
 
 class Extractor
   def self.extract(bucket_name, object_key, binary_name, web_id)
     begin
       status = ExtractionStatus::ERROR
-      error = Hash.new
+      error = Array.new
       s3_put_status = ExtractionStatus::SUCCESS
-      s3_put_error = ""
+      s3_put_error = Array.new
       local_path = "./#{binary_name}"
       s3_path = "messages/#{web_id}.json"
 
@@ -34,8 +35,9 @@ class Extractor
         )
         puts "Getting object #{object_key} with ID #{web_id} from #{bucket_name}"
       rescue StandardError => e
-        error.merge!({"task_id" => web_id, "s3_get_report" => "Error getting object #{object_key} with ID #{web_id} from S3 bucket #{bucket_name}: #{e.message}"})
-        puts error
+        s3_error = "Error getting object #{object_key} with ID #{web_id} from S3 bucket #{bucket_name}: #{e.message}"
+        error.push({"error_type" => ErrorType::S3_GET, "report" => s3_error})
+        puts s3_error
       end
 
       begin
@@ -44,12 +46,14 @@ class Extractor
         status = extraction.status
         puts "status: #{status}"
         puts "error: #{extraction.error}" if status == ExtractionStatus::ERROR
-        error.merge!(extraction.error)
+        error.concat(extraction.error)
         items = extraction.nested_items.map { |o| Hash[o.each_pair.to_a] }
-        return_value = {"web_id" => web_id, "status" => status, "error" => error, "peek_type" => extraction.peek_type, "peek_text" => extraction.peek_text, "nested_items" => items}
+        errors = error.map {|o| Hash[o.each_pair.to_a]}
+        return_value = {"web_id" => web_id, "status" => status, "error" => errors, "peek_type" => extraction.peek_type, "peek_text" => extraction.peek_text, "nested_items" => items}
       rescue
-        error.merge!({"task_id" => web_id, "extraction_process_report" => "Error extracting #{object_key} with ID #{web_id}: #{e.message}"})
-        return_value = {"web_id" => web_id, "status" => ExtractionStatus::ERROR, "error" => error, "peek_type" => PeekType::NONE, "peek_text" => null, "nested_items" => []}
+        error.push({"task_id" => web_id, "extraction_process_report" => "Error extracting #{object_key} with ID #{web_id}: #{e.message}"})
+        errors = error.map {|o| Hash[o.each_pair.to_a]}
+        return_value = {"web_id" => web_id, "status" => ExtractionStatus::ERROR, "error" => errors, "peek_type" => PeekType::NONE, "peek_text" => null, "nested_items" => []}
       end
 
       begin
@@ -61,17 +65,14 @@ class Extractor
         puts "Putting json response for object #{object_key} with ID #{web_id} in S3 bucket #{bucket_name} with key #{s3_path}"
       rescue StandardError => e
         s3_put_status = ExtractionStatus::ERROR
-        s3_put_error = "Error putting json response for object #{object_key} with ID #{web_id} in S3 bucket #{bucket_name}: #{e.message}"
-        puts s3_put_error
-
+        s3_put_error_message = "Error putting json response for object #{object_key} with ID #{web_id} in S3 bucket #{bucket_name}: #{e.message}"
+        s3_put_error.push({"error_type" => ErrorType::S3_PUT, "report" => s3_put_error_message})
+        puts s3_put_error_message
       end
 
-      if s3_put_status == ExtractionStatus::SUCCESS
-        return_value = {"bucket_name" => bucket_name, "object_key" => s3_path}
-      else
-        return_value = {"s3_status" => s3_put_status, "s3_put_report" =>s3_put_error}
-      end
 
+      s3_put_errors = s3_put_error.map {|o| Hash[o.each_pair.to_a]}
+      return_value = {"bucket_name" => bucket_name, "object_key" => s3_path, "s3_status" => s3_put_status, "error" => s3_put_errors}
 
       sqs = Aws::SQS::Client.new(region: region)
 
---
+++ lib/extractor/error_type.rb
@@ -0,0 +1,6 @@
+class ErrorType
+  EXTRACTION = 'extraction_error'
+  PROCESSING = 'processing_error'
+  S3_PUT = 's3_put_error'
+  S3_GET = 's3_get_error'
+end
