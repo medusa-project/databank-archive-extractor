87b886ce91c1e1de43e40f9a4eab0625452ca2f0
Gen Schmitt <gschmitt@illinois.edu>
2021-05-03 11:29:14 -0500

    Added support for xz files

--- lib/extractor/mime_type.rb
+++ lib/extractor/mime_type.rb
@@ -1,5 +1,5 @@
 class MimeType
   ZIP = ["x-zip-compressed", "zip"]
-  NON_ZIP_ARCHIVE = ["x-7z-compressed", "x-tar"]
+  NON_ZIP_ARCHIVE = ["x-7z-compressed", "x-tar", "x-xz"]
   GZIP = ["x-gzip","gzip"]
 end
