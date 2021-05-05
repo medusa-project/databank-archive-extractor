96e8816764e9030ec159a13d352d239bae0d6cc7
Gen Schmitt <gschmitt@illinois.edu>
2021-05-05 08:09:17 -0500

    Added support for rar mime type

--- lib/extractor/mime_type.rb
+++ lib/extractor/mime_type.rb
@@ -1,5 +1,5 @@
 class MimeType
   ZIP = ["x-zip-compressed", "zip"]
-  NON_ZIP_ARCHIVE = ["x-7z-compressed", "x-tar", "x-xz"]
+  NON_ZIP_ARCHIVE = ["x-7z-compressed", "x-tar", "x-xz", "x-rar", "x-rar-compressed"]
   GZIP = ["x-gzip","gzip"]
 end
