b96319b4441b3d56efa5af5d5aeab5d1bb30a311
Gen Schmitt <gschmitt@illinois.edu>
2021-05-05 14:31:43 -0500

    Added support for gtar files

--- lib/extractor/mime_type.rb
+++ lib/extractor/mime_type.rb
@@ -1,5 +1,5 @@
 class MimeType
   ZIP = ["x-zip-compressed", "zip"]
-  NON_ZIP_ARCHIVE = ["x-7z-compressed", "x-tar", "x-xz", "x-rar", "x-rar-compressed"]
+  NON_ZIP_ARCHIVE = ["x-7z-compressed", "x-tar", "x-xz", "x-rar", "x-rar-compressed", "x-gtar"]
   GZIP = ["x-gzip","gzip"]
 end
